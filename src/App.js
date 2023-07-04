457942a518655821b123ebf4eac9fcf4a68fba53
Bar Goldenstein <bargolden12@gmail.com>
2023-07-04 10:43:59 +0300

    init repo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export function Tenor() {
+  	const [url, setUrl] = useState('');
+	const { param } = useParams();
+	useEffect(() => {
+			const fetchGif = async () => { // cors-anywhere.herokuapp.com is a CORS proxy
+				axios.get(`https://cors-anywhere.herokuapp.com/https://tenor.com/view/${param}`)
+				.then(response => {
+					const matches = response.data.match(/https:\/\/media\.tenor\.com\/[^/]+\/[^/]+\.gif/g);
+					setUrl(matches[0]);
+				}).catch(error => {
+					console.error('Error fetching GIF:', error);
+				});
+			};
+			fetchGif();
+	}, [param]);
+
+	return <script>{window.location.href = url}</script>;
 }
-
-export default App;
+export function Discord() {
+	const { param1, param2, param3 } = useParams();
+	const url = `https://media.discordapp.net/attachments/${param1}/${param2}/${param3}`;
+	return <script>{window.location.href = url}</script>;
+}
--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { Tenor, Discord } from './App';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+createRoot(document.getElementById('root')).render(
+	<Router>
+		<Routes>
+			<Route path="/https://tenor.com/view/:param" element={<Tenor />} />
+			<Route path="/https://media.discordapp.net/attachments/:param1/:param2/:param3" element={<Discord />} />
+		</Routes>
+	</Router>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
